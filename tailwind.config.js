3aa3da403950a26d0257aacb71117a33923f84cd
Malek Kazal <ghazal.malek@gmail.com>
2024-05-28 09:16:03 -0400

    app cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,47 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [tailwindcssAnimate, daisyui],
+
+  daisyui: {
+    themes: false,
+    darkTheme: 'dark',
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+  },
+};
