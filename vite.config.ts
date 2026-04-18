002e92cbe639a401c4604ae8b29d1a2014f27437
MalekGhazal <ghazal.malek@gmail.com>
2026-04-18 11:51:54 +0400

    setup github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio-v4/',
   plugins: [react()],
   server: {
     port: 3000,
